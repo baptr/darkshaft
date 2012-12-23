dadf3d2441978b5fc7c381107540d32d0ae482da
Bryce Petrini <baptr01@gmail.com>
2012-12-22 18:07:06 -0600

    Add desktop texture packing to build

--- main/src/com/baptr/darkshaft/utils/DarkshaftTexturePacker.java
+++ main/src/com/baptr/darkshaft/utils/DarkshaftTexturePacker.java
@@ -6,15 +6,12 @@ import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 /**
  * Packs single images into image atlases.
  */
-public class DarkshaftTexturePacker
-{
-    private static final String INPUT_DIR = "etc/images";
-    private static final String OUTPUT_DIR = "../darkshaft/main/assets/image-atlases";
+public class DarkshaftTexturePacker {
+    private static final String INPUT_DIR = "../main/images";
+    private static final String OUTPUT_DIR = "../main/assets/image-atlases";
     private static final String PACK_FILE = "pages-info";
 
-    public static void main(
-        String[] args )
-    {
+    public static void main( String[] args ) {
         // create the packing's settings
         Settings settings = new Settings();
 
