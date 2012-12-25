cf9ff0369dc6caa22ec8d34e1d7d7c1020f78eb2
Ryan <rjr5838@gmail.com>
2012-12-24 20:08:04 -0500

    Added zooming to the CameraInputProcessor. You can use the mouse's scroll wheel to zoom in and out now.

--- main/src/com/baptr/darkshaft/input/CameraInputProcessor.java
+++ main/src/com/baptr/darkshaft/input/CameraInputProcessor.java
@@ -9,6 +9,9 @@ public class CameraInputProcessor extends AbstractInputProcessor {
 
     int dragX;
     int dragY;
+    final float MIN_ZOOM = .4f;
+    final float MAX_ZOOM = 3f;
+    
     OrthographicCamera camera;
     
     public CameraInputProcessor(OrthographicCamera camera) {
@@ -41,8 +44,17 @@ public class CameraInputProcessor extends AbstractInputProcessor {
     }
 
     @Override
+    //  1 == Scroll Down
+    // -1 == Scroll Up
     public boolean scrolled(int direction) {
-        // TODO Auto-generated method stub
+        //Gdx.app.log( Darkshaft.LOG, "scrolled: " + getName() + " direction: " + direction);
+        // Zoom out
+        if(direction > 0 && camera.zoom < MAX_ZOOM)
+            camera.zoom += .1f;
+        // Zoom in
+        if(direction < 0 && camera.zoom > MIN_ZOOM)
+            camera.zoom -= .1f;
+        camera.update();
         return true;
     }
 
@@ -56,7 +68,7 @@ public class CameraInputProcessor extends AbstractInputProcessor {
     @Override
     public boolean touchDragged(int x, int y, int pointer) {
 
-        Gdx.app.log( Darkshaft.LOG, "touchDragged: " + getName() + " to: (" + x + ", " + y + "), pointer: " + pointer);
+        //Gdx.app.log( Darkshaft.LOG, "touchDragged: " + getName() + " to: (" + x + ", " + y + "), pointer: " + pointer);
         this.camera.translate(dragX-x, y-dragY);
         camera.update();
         dragX = x;
