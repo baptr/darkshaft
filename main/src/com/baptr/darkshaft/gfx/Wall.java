70c857b54777e8cd66fa84693c4d3931561504f4
Bryce Petrini <baptr01@gmail.com>
2012-12-26 03:16:32 -0600

    Few build fixes

--- main/src/com/baptr/darkshaft/gfx/Wall.java
+++ main/src/com/baptr/darkshaft/gfx/Wall.java
@@ -3,7 +3,7 @@ package com.baptr.darkshaft.gfx;
 public class Wall extends Defense {
 
     public Wall(float x, float y, int facing) {
-        super("wall.png", x, y);
+        super(5, x, y);
     }
 
 }
